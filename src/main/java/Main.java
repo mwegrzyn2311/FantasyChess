91247afd16a9980a262dcf89c3e685d1d9ad711e
Michal23 <michal.wegrzyn2137@gmail.com>
2020-03-13 20:56:31 +0100

    aha

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import GUI.ChessFrame;
 import java.io.IOException;
 
 public class Main {
+    // TODO: Bug with trying to mate(?) where sometimes, beaten piece can revive?
     public static void main(String[] args) {
         try {
             ChessFrame frame = new ChessFrame();
